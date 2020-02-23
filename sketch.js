0f78b791cbc995f97c066901d9f31da483a4f22a
PSteely <peter.steely@gmail.com>
2020-02-23 09:51:08 +0000

    Basics Working

--- sketch.js
+++ sketch.js
@@ -1,11 +1,46 @@
+// Initialize the Image Classifier method with MobileNet. A callback needs to be passed.
+let classifier;
 
+// A variable to hold the image we want to classify
+let img;
+let video;
+
+
+function preload() {
+  classifier = ml5.imageClassifier('MobileNet');
+  //img = loadImage('images/eagle.jpeg');
+}
 
 function setup() {
-  canvas =  createCanvas(640, 480);
+  canvas = createCanvas(1000, 900);
   canvas.parent('canvas');
+  video = createCapture(VIDEO);
+  video.hide();
+  classifier.classify(video, gotResult);
+  image(video, 0, 0);
+}
+
+function draw() {
+  image(video, 0, 0, width, height);
+  classifier.classify(video, gotResult);
 
 }
-function draw() {
-    background (0);
 
+// A function to run when we get any errors and the results
+function gotResult(error, results) {
+  // Display error in the console
+  if (error) {
+    console.error(error);
+  } else {
+    // The results are in an array ordered by confidence.
+    //console.log(results);
+
+    const labelP = document.getElementById("label")
+    labelP.innerText = "Label: " + results[0].label;
+
+    const confidenceP = document.getElementById("confidence");
+    confidenceP.innerText = "Confidence: " + results[0].confidence;
+
+
+  }
 }
